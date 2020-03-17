7b58700aced963ec77787fb5060ee10b771995d1
guillemdb <guillem.db@gmail.com>
2020-03-17 10:06:46 +0100

    Another try at docs

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -24,15 +24,16 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../../fragile"))
 sys.setrecursionlimit(1500)
 MOCK_MODULES = ["pandas", "holoviews.streams",
-                "holoviews", "PIL", "plangym", "numba", "pyarrow"]
+                "holoviews", "PIL", "numba", "pyarrow"] #, "plangym", "plangym.env"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- Project information -----------------------------------------------------
 
 project = "Fragile"
-copyright = "2020, Guillem Duran, Sergio Hernandez"
+copyright = "2020, Fragile Technologies"
 author = "Guillem Duran, Sergio Hernandez"
 
 # The short X.Y version
@@ -62,6 +63,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
     "m2r",
+    #"sphinx-rtd-theme",
 ]
 # Napoleon settings
 napoleon_google_docstring = True
@@ -109,7 +111,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
